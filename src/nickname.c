eeda1116b861b2fa2e58f8bad2fa904524ac09a1
ikruns <ikruns@gmail.com>
2019-04-13 09:00:42 +0800

    fix free const char * error

--- src/nickname.c
+++ src/nickname.c
@@ -19,73 +19,104 @@ const NICKNAMEOPTION NICKNAMEOPTION_DEFAULT = {.nicknameLength = 8,
    str2 [in]
    concatenate str1 and str2 then return, len is return string's length
  */
-char* concatenateString(int* len, const char* str1, const char* str2) {
+char *concatenateString(int *len, const char *str1, const char *str2)
+{
   int len1 = 0, len2 = 0, len3 = 0;
-  while (str1[len1] != '\0') {
+  while (str1[len1] != '\0')
+  {
     len1++;
   }
 
-  while (str2[len2] != '\0') {
+  while (str2[len2] != '\0')
+  {
     len2++;
   }
 
   len3 = len1 + len2;
   *len = len3;
-  char* ret = (char*)malloc(sizeof(char) * len3);
-  for (int i = 0; i < len1; ++i) {
+  char *ret = (char *)malloc(sizeof(char) * len3);
+  for (int i = 0; i < len1; ++i)
+  {
     ret[i] = str1[i];
   }
-  for (int i = 0; i < len2; ++i) {
+  for (int i = 0; i < len2; ++i)
+  {
     ret[len1 + i] = str2[i];
   }
 
   return ret;
 }
 
-char* getNickName(NICKNAMEOPTION* nicknameOption) {
-  char* tempDict;
+char *getNickName(NICKNAMEOPTION *nicknameOption)
+{
+  char *tempDict;
   int len = 0;
-  
+
   /* 选择字典 */
   bool upper = nicknameOption->allowUpperChar;
   bool symbol = nicknameOption->allowSymbool;
   bool number = nicknameOption->allowNumber;
-  if (upper && symbol && number) {
+  if (upper && symbol && number)
+  {
     tempDict = concatenateString(
         &len,
         concatenateString(&len, concatenateString(&len, dictLower, dictUpper),
                           dictSymbol),
         dictNumber);
-  } else if (upper && symbol && !number) {
+  }
+  else if (upper && symbol && !number)
+  {
     tempDict = concatenateString(
         &len, concatenateString(&len, dictLower, dictUpper), dictSymbol);
-  } else if (upper && !symbol && number) {
+  }
+  else if (upper && !symbol && number)
+  {
     tempDict = concatenateString(
         &len, concatenateString(&len, dictLower, dictUpper), dictNumber);
-  } else if (!upper && symbol && number) {
+  }
+  else if (!upper && symbol && number)
+  {
     tempDict = concatenateString(
         &len, concatenateString(&len, dictLower, dictSymbol), dictNumber);
-  } else if (!upper && !symbol && number) {
+  }
+  else if (!upper && !symbol && number)
+  {
     tempDict = concatenateString(&len, dictLower, dictNumber);
-  } else if (!upper && symbol && !number) {
+  }
+  else if (!upper && symbol && !number)
+  {
     tempDict = concatenateString(&len, dictLower, dictSymbol);
-  } else if (upper && !symbol && !number) {
+  }
+  else if (upper && !symbol && !number)
+  {
     tempDict = concatenateString(&len, dictLower, dictUpper);
-  } else if (!upper && !symbol && !number) {
+  }
+  else if (!upper && !symbol && !number)
+  {
     tempDict = dictLower;
     len = sizeof(tempDict) / sizeof(tempDict[0]);
-  } else {
+  }
+  else
+  {
     tempDict = dictLower;
     len = sizeof(tempDict) / sizeof(tempDict[0]);
   }
 
   srand((unsigned int)time(NULL));
-  for (int number = 0; number < nicknameOption->numberOfNickname; ++number) {
-    for (int length = 0; length < nicknameOption->nicknameLength; ++length) {
+  for (int number = 0; number < nicknameOption->numberOfNickname; ++number)
+  {
+    for (int length = 0; length < nicknameOption->nicknameLength; ++length)
+    {
       printf("%c", tempDict[rand() % len]);
     }
   }
 
-  free(tempDict);
+  printf("\n");
+
+  if (tempDict != dictSymbol && tempDict != dictLower && tempDict != dictNumber && tempDict != dictUpper)
+  {
+    free(tempDict);
+  }
+
   return "";
 }
